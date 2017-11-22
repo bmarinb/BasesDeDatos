a6dffb3a38d57405e54f9cf55e08f19e6f93d5bf
stgoa <sarmstrong@uc.cl>
2017-11-22 02:13:37 -0300

    con lo mio

--- main.py
+++ main.py
@@ -3,45 +3,55 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 from bson import json_util
 import json
+
 with open('users.json') as file:
-	users_json= json.load(file)
+    users_json = json.load(file)
 with open('messages.json')as file:
-	msgs_json = json.load(file)
+    msgs_json = json.load(file)
 app = Flask(__name__)
 
-
-client = MongoClient() #localhost 27017
-db = client.grupo11 #mi base de datos se llama test
-users = db.users #dentro de test, una coleccion es users
+client = MongoClient()  # localhost 27017
+db = client.grupo11  # mi base de datos se llama test
+users = db.users  # dentro de test, una coleccion es users
 # users.insert_many(users_json)
-msgs = db.msgs #dentro de test, otra coleccion es tweets
+msgs = db.msgs  # dentro de test, otra coleccion es tweets
+
+
 # msgs.insert_many(msgs_json)
 @app.route("/")
 def hello():
     return "Hello World!"
 
 
-
 @app.route("/users/<id>")
-def find_users(id=None):	
-	result = {}
-	artist = users.find({"id":int(id)},{"_id":0})
-	artist = next(artist)
-	artist_msgs = msgs.find({"sender":int(id)},{"_id":0})
-	artist_msgs = [msg for msg in artist_msgs]	
-	return jsonify(artist=artist, msgs=artist_msgs)	
+def find_users(id=None):
+    result = {}
+    artist = users.find({"id": int(id)}, {"_id": 0})
+    artist = next(artist)
+    artist_msgs = msgs.find({"sender": int(id)}, {"_id": 0})
+    artist_msgs = [msg for msg in artist_msgs]
+    return jsonify(artist=artist, msgs=artist_msgs)
 
 
 @app.route("/msgs/<mid>")
 def find_msgs(mid=None):
-	a = int('5a14f1e3e3928a3d598f67df',16)
-	msg_id = hex(a+int(mid))
-	msg_id = msg_id[2:]
-	results = msgs.find({"_id": ObjectId(msg_id)},{"_id":0})
-	return jsonify("msgs", [msg for msg in results])	
+    a = int('5a14f1e3e3928a3d598f67df', 16)
+    msg_id = hex(a + int(mid))
+    msg_id = msg_id[2:]
+    results = msgs.find({"_id": ObjectId(msg_id)}, {"_id": 0})
+    return jsonify("msgs", [msg for msg in results])
 
 
+@app.route("/tweets/<mid>/<uid>")
+def find_tweets_by_user(mid=None, uid=None):
+    results = msgs.find({"sender": int(mid), "receptant": int(uid)})
+    print(mid)
+    print(uid)
+    print([user for user in results])
+    # esta id que genera mongo no es serializable por python, por lo que pongo _id: 0 para que no lo muestre
+    # nombre que se muestra
+    return jsonify("tweet", [tuit for tuit in results])
 
 
 if __name__ == '__main__':
-	app.run(port=2000)
+    app.run(port=2000)
